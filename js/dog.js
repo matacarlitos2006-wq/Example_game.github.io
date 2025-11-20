2d124069f77a0371cad9e630f41c96cdf5167968
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2025-11-19 18:26:21 -0700

    Update dog.js

--- js/dog.js
+++ js/dog.js
@@ -1,1 +1,21 @@
+class Dog {
+  constructor(x, y, img) {
+    this.x = x;
+    this.y = y;
+    this.img = img;
+    this.width = 40;
+    this.height = 40;
+    this.alive = true;
+  }
+
+  update() {
+    // For now, dogs don't move. You can add AI later.
+  }
+
+  draw(ctx) {
+    if (this.alive) {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+  }
+}
 
